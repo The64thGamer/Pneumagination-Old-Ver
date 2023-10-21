c434e53e91e85839f3401e50f6fa33f12421fca2
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-10-20 22:48:53 -0500

    Changed DTU tester to let you use number keys

--- Assets/Scripts/DEAD Robot/DEAD_DTU_Tester.cs
+++ Assets/Scripts/DEAD Robot/DEAD_DTU_Tester.cs
@@ -5,22 +5,103 @@ using UnityEngine;
 public class DEAD_DTU_Tester : MonoBehaviour
 {
     [SerializeField] DEAD_Interface dead_Interface;
-    [SerializeField] DEAD_Tester_Values[] testingValues;
+    [Header("Use NumKeys to test, indexes are tested DTU bits.")]
+    [SerializeField] int index1;
+    [SerializeField] int index2;
+    [SerializeField] int index3;
+    [SerializeField] int index4;
+    [SerializeField] int index5;
+    [SerializeField] int index6;
+    [SerializeField] int index7;
+    [SerializeField] int index8;
+    [SerializeField] int index9;
+    [SerializeField] int index0;
     private void Update()
     {
         if(dead_Interface != null)
         {
-            for (int i = 0; i < testingValues.Length; i++)
+            for (int i = 0; i < 10; i++)
             {
-                dead_Interface.SetData(testingValues[i].index, testingValues[i].value ? 1f : 0f);
+                int index = 0;
+                bool value = false;
+                switch (i)
+                {
+                    case 1:
+                        index = index1;
+                        if (Input.GetKey(KeyCode.Alpha1))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 2:
+                        index = index2;
+                        if (Input.GetKey(KeyCode.Alpha2))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 3:
+                        index = index3;
+                        if (Input.GetKey(KeyCode.Alpha3))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 4:
+                        index = index4;
+                        if (Input.GetKey(KeyCode.Alpha4))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 5:
+                        index = index5;
+                        if (Input.GetKey(KeyCode.Alpha5))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 6:
+                        index = index6;
+                        if (Input.GetKey(KeyCode.Alpha6))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 7:
+                        index = index7;
+                        if (Input.GetKey(KeyCode.Alpha7))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 8:
+                        index = index8;
+                        if (Input.GetKey(KeyCode.Alpha8))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 9:
+                        index = index9;
+                        if (Input.GetKey(KeyCode.Alpha9))
+                        {
+                            value = true;
+                        }
+                        break;
+                    case 0:
+                        index = index0;
+                        if (Input.GetKey(KeyCode.Alpha0))
+                        {
+                            value = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                dead_Interface.SetData(index, value ? 1f : 0f);
             }
         }
     }
 
-    [System.Serializable]
-    public class DEAD_Tester_Values
-    {
-        public int index;
-        public bool value;
-    }
 }
