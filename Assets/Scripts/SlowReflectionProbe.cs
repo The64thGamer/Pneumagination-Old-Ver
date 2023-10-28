b167a9994ded27debe6def304d6f491a917e9dbe
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-10-27 20:52:45 -0500

    Added slowly updating reflection probes

---
+++ Assets/Scripts/SlowReflectionProbe.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowReflectionProbe : MonoBehaviour
+{
+    float timer;
+    float reflectionTime;
+    ReflectionProbe probe;
+    void Start()
+    {
+        reflectionTime = PlayerPrefs.GetInt("Settings: ReflectionProbeUpdates");
+        probe = this.GetComponent<ReflectionProbe>();
+        if(reflectionTime == 0 || probe == null)
+        {
+            Destroy(this);
+        }
+        probe.refreshMode = UnityEngine.Rendering.ReflectionProbeRefreshMode.ViaScripting;
+        timer = Random.Range(0, reflectionTime);
+    }
+
+    
+    void Update()
+    {
+        if(timer <= 0)
+        {
+            timer = reflectionTime;
+            probe.RenderProbe();
+        }
+        timer -= Time.deltaTime;
+    }
+}
