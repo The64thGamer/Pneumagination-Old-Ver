55f6a0248e7b950e9e525832e9ab317468aec6a7
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-10-27 23:55:50 -0500

    Added saving settings and applying them in-game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,8 +4,9 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using static UnityEditor.UIElements.CurveField;
 
-public class Test : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     [Header("Menu")]
     [SerializeField] UIDocument document;
@@ -13,6 +14,15 @@ public class Test : MonoBehaviour
     bool generateRandomName = true;
 
     bool loadingScene;
+
+    //Const
+    const string part1 = "Startup: SettingsPart1";
+    const string dlssMode = "Settings: DLSS";
+    const string lightingMode = "Settings: Lighting";
+    const string reflectionsMode = "Settings: Reflections";
+    const string sampleCount = "Settings: Sample Count";
+    const string bounceCount = "Settings: Bounce Count";
+    const string probeTimer = "Settings: Probe Timer";
 
     void OnEnable()
     {
@@ -28,10 +38,69 @@ public class Test : MonoBehaviour
         document.rootVisualElement.Q<Button>("BackFromSettings").clicked += () => SwitchMenu(0);
         document.rootVisualElement.Q<Toggle>("RandomNames").RegisterValueChangedCallback(ToggleRandomName);
 
+        //Settings
+        document.rootVisualElement.Q<DropdownField>("DLSS").RegisterValueChangedCallback(SaveAllSettings);
+        document.rootVisualElement.Q<DropdownField>("Renderer").RegisterValueChangedCallback(SaveAllSettings);
+        document.rootVisualElement.Q<VisualElement>("SampleCount").Q<SliderInt>("Slider").RegisterValueChangedCallback(SaveAllSettings);
+        document.rootVisualElement.Q<VisualElement>("Bounces").Q<SliderInt>("Slider").RegisterValueChangedCallback(SaveAllSettings);
+        document.rootVisualElement.Q<VisualElement>("ReflectionProbes").Q<SliderInt>("Slider").RegisterValueChangedCallback(SaveAllSettings);
+
         int seed = Random.Range(int.MinValue, int.MaxValue);
         int age = Random.Range(0, 80);
         document.rootVisualElement.Q<TextField>("FirstName").value = Name_Generator.GenerateFirstName(seed, age);
         document.rootVisualElement.Q<TextField>("LastName").value = Name_Generator.GenerateLastName(seed);
+
+        if(PlayerPrefs.GetInt(part1) == 0)
+        {
+            FirstTimeBootingSettings();
+        }
+        LoadAllSettings();
+    }
+
+    void FirstTimeBootingSettings()
+    {
+        PlayerPrefs.SetInt(part1, 1);
+
+        PlayerPrefs.SetInt(dlssMode, 1);
+        PlayerPrefs.SetInt(lightingMode, 1);
+        PlayerPrefs.SetInt(reflectionsMode, 0);
+        PlayerPrefs.SetInt(sampleCount, 2);
+        PlayerPrefs.SetInt(bounceCount, 1);
+        PlayerPrefs.SetInt(probeTimer, 30);
+    }
+    void SaveAllSettings(UnityEngine.UIElements.ChangeEvent<int> toggle)
+    {
+        SaveAllSettings();
+    }
+    void SaveAllSettings(UnityEngine.UIElements.ChangeEvent<string> toggle)
+    {
+        SaveAllSettings();
+    }
+
+    void SaveAllSettings()
+    {
+        PlayerPrefs.SetInt(dlssMode, document.rootVisualElement.Q<DropdownField>("DLSS").index);
+        PlayerPrefs.SetInt(lightingMode, document.rootVisualElement.Q<DropdownField>("LightingMode").index);
+        PlayerPrefs.SetInt(reflectionsMode, document.rootVisualElement.Q<DropdownField>("ReflectionsMode").index);
+        PlayerPrefs.SetInt(sampleCount, document.rootVisualElement.Q<VisualElement>("SampleCount").Q<SliderInt>("Slider").value);
+        PlayerPrefs.SetInt(bounceCount, document.rootVisualElement.Q<VisualElement>("Bounces").Q<SliderInt>("Slider").value);
+        PlayerPrefs.SetInt(probeTimer, document.rootVisualElement.Q<VisualElement>("ReflectionProbes").Q<SliderInt>("Slider").value);
+    }
+
+    void LoadAllSettings()
+    {
+        int dlssLoad = PlayerPrefs.GetInt(dlssMode);
+        int lightingLoad = PlayerPrefs.GetInt(lightingMode);
+        int reflectionsLoad = PlayerPrefs.GetInt(reflectionsMode);
+        int sampleLoad = PlayerPrefs.GetInt(sampleCount);
+        int bounceLoad = PlayerPrefs.GetInt(bounceCount);
+        int probeLoad = PlayerPrefs.GetInt(probeTimer);
+        document.rootVisualElement.Q<DropdownField>("DLSS").index = dlssLoad;
+        document.rootVisualElement.Q<DropdownField>("LightingMode").index = lightingLoad;
+        document.rootVisualElement.Q<DropdownField>("ReflectionsMode").index = reflectionsLoad;
+        document.rootVisualElement.Q<VisualElement>("SampleCount").Q<SliderInt>("Slider").value = (sampleLoad);
+        document.rootVisualElement.Q<VisualElement>("Bounces").Q<SliderInt>("Slider").value = (bounceLoad);
+        document.rootVisualElement.Q<VisualElement>("ReflectionProbes").Q<SliderInt>("Slider").value = (probeLoad);
     }
 
     void ToggleRandomName(UnityEngine.UIElements.ChangeEvent<bool> toggle)
