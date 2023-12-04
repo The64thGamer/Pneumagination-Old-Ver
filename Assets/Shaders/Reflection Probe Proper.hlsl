60ca067884511b47ecd1e4e6d35e0158172065f1
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-12-04 01:21:24 -0600

    Other Changes

--- Assets/Shaders/Reflection Probe Proper.hlsl
+++ Assets/Shaders/Reflection Probe Proper.hlsl
@@ -1,21 +1,21 @@
-float3 viewDirWS = normalize(ViewDirWS);
+Vector3 viewDirWS = normalize(ViewDirWS);
 
-float3 normalWS = normalize(NormalWS);
+Vector3 normalWS = normalize(NormalWS);
 
-float3 reflDir = normalize(reflect(-viewDirWS, normalWS));
+Vector3 reflDir = normalize(reflect(-viewDirWS, normalWS));
 
 
 
-float3 factors = ((reflDir > 0 ? unity_SpecCube0_BoxMax.xyz : unity_SpecCube0_BoxMin.xyz) - PositionWS) / reflDir;
+Vector3 factors = ((reflDir > 0 ? unity_SpecCube0_BoxMax.xyz : unity_SpecCube0_BoxMin.xyz) - PositionWS) / reflDir;
 
 float scalar = min(min(factors.x, factors.y), factors.z);
 
-float3 uvw = reflDir * scalar + (PositionWS - unity_SpecCube0_ProbePosition.xyz);
+Vector3 uvw = reflDir * scalar + (PositionWS - unity_SpecCube0_ProbePosition.xyz);
 
 
 
-float4 sampleRefl = SAMPLE_TEXTURECUBE_LOD(unity_SpecCube0, samplerunity_SpecCube0, uvw, LOD);
+Vector4 sampleRefl = SAMPLE_TEXTURECUBE_LOD(unity_SpecCube0, samplerunity_SpecCube0, uvw, LOD);
 
-float3 specCol = DecodeHDREnvironment(sampleRefl, unity_SpecCube0_HDR);
+Vector3 specCol = DecodeHDREnvironment(sampleRefl, unity_SpecCube0_HDR);
 
 Color = specCol;
