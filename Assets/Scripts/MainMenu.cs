c32953db0ae865e1de1a8370e7b4d8ff11e6b6ab
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-12-08 17:57:25 -0600

    More manual changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
