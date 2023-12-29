613b28f238fe93329f834b8dd23e9a17107daf58
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-12-28 19:15:34 -0600

    Removed LOD scripts and added model detail scripts

---
+++ Assets/Scripts/LOD Adjuster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LODAdjuster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Renderer[] rends = this.GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < rends.Length; i++)
+        {
+            if (rends[i].name.Contains("LOD0"))
+            {
+                rends[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                rends[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
