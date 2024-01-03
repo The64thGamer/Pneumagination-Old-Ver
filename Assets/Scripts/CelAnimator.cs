0e4c95d549b3863ad06ef27fe3c7d68ac3922979
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-01-02 23:24:27 -0600

    Added Cel Outline Shaders

---
+++ Assets/Scripts/CelAnimator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFx.Outline;
+
+public class CelAnimator : MonoBehaviour
+{
+    OutlineEffect effect;
+
+    private void OnEnable()
+    {
+        effect = Camera.main.GetComponent<OutlineEffect>();
+
+        effect.OutlineLayers[0].Add(this.gameObject);
+        effect.OutlineLayers[1].Add(this.gameObject);
+    }
+
+    private void OnDisable()
+    {
+        effect.OutlineLayers[0].Remove(this.gameObject);
+        effect.OutlineLayers[1].Remove(this.gameObject);
+    }
+
+}
