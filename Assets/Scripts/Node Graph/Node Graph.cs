a999873a60395ec8b791bf4b7ca3cec6d23164fc
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-02-09 16:48:07 -0600

    Node Graph Beginnings

---
+++ Assets/Scripts/Node Graph/Node Graph.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodeGraph : MonoBehaviour
+{
+    [SerializeField] Transform backingPlane;
+    [SerializeField] Camera mainCam;
+    
+    int zoomLevel = 10;
+    Vector2 screenRes;
+
+    const int minZoomLevel = 5;
+    const int maxZoomLevel = 100;
+
+    void Awake()
+    {
+        screenRes = new Vector2(Screen.width, Screen.height);
+        UpdateBackingPlane();
+    }
+
+    void Update()
+    {
+        if (screenRes.x != Screen.width || screenRes.y != Screen.height)
+        {
+            screenRes.x = Screen.width;
+            screenRes.y = Screen.height;
+            UpdateBackingPlane();
+        }
+
+        if(Input.mousePositionDelta != Vector3.zero && Input.GetMouseButton(0))
+        {
+            mainCam.transform.position -= Input.mousePositionDelta * zoomLevel / Mathf.Min(screenRes.x, screenRes.y);
+            UpdateBackingPlane();
+        }
+
+        if(Input.mouseScrollDelta != Vector2.zero)
+        {
+            zoomLevel = Mathf.Clamp(zoomLevel - (int)Input.mouseScrollDelta.y*5,minZoomLevel,maxZoomLevel);
+            UpdateBackingPlane();
+        }
+    }
+
+    void UpdateBackingPlane()
+    {
+        mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y, -zoomLevel);
+        float pos = (mainCam.nearClipPlane + zoomLevel);
+        backingPlane.position = mainCam.transform.position + (mainCam.transform.forward * pos);
+        float h = Mathf.Tan(mainCam.fieldOfView * Mathf.Deg2Rad * 0.5f) * pos * 2f;
+        backingPlane.localScale = new Vector3(h * mainCam.aspect, h, 1f);
+    }
+}
