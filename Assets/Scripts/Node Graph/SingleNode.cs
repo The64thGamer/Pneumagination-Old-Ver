7f8db3287ac8aff9fe7aac58adfc237a102f8c15
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-02-09 20:43:37 -0600

    Box Stretching & Shadows

--- Assets/Scripts/Node Graph/SingleNode.cs
+++ Assets/Scripts/Node Graph/SingleNode.cs
@@ -6,16 +6,24 @@ using UnityEngine.UIElements;
 
 public class SingleNode : MonoBehaviour
 {
-    RenderTexture rt;
-    TMP_Text label;
+    [SerializeField] TMP_Text label;
+    [SerializeField] SkinnedMeshRenderer skin;
+
     void Start()
     {
         UpdateName(name);
+        UpdateSize(2, Random.Range(2,6));
     }
 
     void UpdateName(string newName)
     {
         label.text = newName;
+    }
+
+    void UpdateSize(int x, int y)
+    {
+        skin.SetBlendShapeWeight(0, x);
+        skin.SetBlendShapeWeight(1, y);
     }
 
 }
