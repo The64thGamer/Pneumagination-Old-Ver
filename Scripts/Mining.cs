29b8351bec2c1cc74e630384aacb53f4f4cfc006
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-03-16 14:17:26 -0500

    Destroying functions but rarely works

--- Scripts/Mining.cs
+++ Scripts/Mining.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Mining : Node3D
 {
-    [Export] WorldGen worldGen;
+    [Export] public WorldGen worldGen;
 
     public override void _PhysicsProcess(double delta)
     {
@@ -15,17 +15,12 @@ public partial class Mining : Node3D
             Godot.Collections.Dictionary result = spaceState.IntersectRay(query);
             if (result.Count > 0)
             {
-                GD.Print("Hit at Triangle: ", result["face_index"]);
                 Sprite3D sprite = new Sprite3D();
                 sprite.Texture = (Texture2D)GD.Load("res://Textures/testtexture.png");
                 GetTree().Root.AddChild(sprite);
                 sprite.GlobalPosition = (Vector3)result["position"];
 
-
-                if ((CollisionObject3D)result["collider"] is CollisionObject3D collisionObj && (int)result["face_index"] >= 0)
-                {
-                    worldGen.DestroyBlock(collisionObj.GetParent() as Node3D, Mathf.FloorToInt(((int)result["face_index"]) / 12.0f)); //"/12.0f" for each face of the cube
-                }
+                worldGen.DestroyBlock(((Node3D)result["collider"]).GetParent().GetParent() as Node3D, Mathf.FloorToInt(((int)result["face_index"]) / 12.0f)); //"/12.0f" for each face of the cube
 
             }
         }
