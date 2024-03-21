e6b73ed8a75ffb106355f6895863b37283535c33
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-03-21 14:01:23 -0500

    Darkened sky and fog colors when going below Y level 0 to reduce light leaking

--- Scripts/EnvironmentController.cs
+++ Scripts/EnvironmentController.cs
@@ -1,15 +1,19 @@
 using Godot;
 using System;
 
-public partial class EnvironmentController : Node
+public partial class EnvironmentController : WorldEnvironment
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [Export] Gradient fogColor;
+    [Export] Gradient skyColor;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta)
+    {
+        float range = GetClampedRange(-200, 0, PlayerMovement.currentPosition.Y);
+        Environment.FogLightColor = fogColor.Sample(range);
+        Environment.BackgroundColor = skyColor.Sample(range);
+    }
+    float GetClampedRange(float lowerBound, float upperBound, float pos)
+    {
+        return Mathf.Clamp((pos - lowerBound) / (upperBound - lowerBound), 0, 1);
+    }
 }
