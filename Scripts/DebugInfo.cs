ce5e6a480f0b7e08cbb09035c8a125c186c03b4b
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-03-21 03:07:09 -0500

    Added position to debug menu

--- Scripts/DebugInfo.cs
+++ Scripts/DebugInfo.cs
@@ -6,7 +6,7 @@ public partial class DebugInfo : Label
     string text = "";
     public override void _Process(double delta)
     {
-        text = "\n " + (string)ProjectSettings.GetSetting("application/config/name") + " v" + (string)ProjectSettings.GetSetting("application/config/version") + " Seed- " + WorldGen.seedA.ToString("D10")+ WorldGen.seedB.ToString("D10") + WorldGen.seedC.ToString("D10") + WorldGen.seedD.ToString("D10");
+        text = "\n " + (string)ProjectSettings.GetSetting("application/config/name") + " v" + (string)ProjectSettings.GetSetting("application/config/version") + " Seed- " + WorldGen.seedA.ToString("D10")+ WorldGen.seedB.ToString("D10") + WorldGen.seedC.ToString("D10") + WorldGen.seedD.ToString("D10") + " X" + PlayerMovement.currentPosition.Y + " Y" +PlayerMovement.currentPosition.X+ " Z"+ PlayerMovement.currentPosition.Z;
         if(this.Text != text)
         {
             this.Text = text;
