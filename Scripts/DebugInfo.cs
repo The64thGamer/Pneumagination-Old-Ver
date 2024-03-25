9abbeb3cf55ef864999b67d557a9d236c322632d
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-03-25 14:34:52 -0500

    Added hour display to debug info

--- Scripts/DebugInfo.cs
+++ Scripts/DebugInfo.cs
@@ -1,12 +1,13 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class DebugInfo : Label
 {
     string text = "";
     public override void _Process(double delta)
     {
-        text = "\n " + (string)ProjectSettings.GetSetting("application/config/name") + " v" + (string)ProjectSettings.GetSetting("application/config/version") + " Seed- " + WorldGen.seedA.ToString("D10")+ WorldGen.seedB.ToString("D10") + WorldGen.seedC.ToString("D10") + WorldGen.seedD.ToString("D10") + " X" + (int)PlayerMovement.currentPosition.X + " Y" + (int)PlayerMovement.currentPosition.Y + " Z"+ (int)PlayerMovement.currentPosition.Z;
+        text = "\n " + (string)ProjectSettings.GetSetting("application/config/name") + " v" + (string)ProjectSettings.GetSetting("application/config/version") + " Seed- " + WorldGen.seedA.ToString("D10")+ WorldGen.seedB.ToString("D10") + WorldGen.seedC.ToString("D10") + WorldGen.seedD.ToString("D10") + " X" + (int)PlayerMovement.currentPosition.X + " Y" + (int)PlayerMovement.currentPosition.Y + " Z"+ (int)PlayerMovement.currentPosition.Z + " " + DateTime.Today.Add(TimeSpan.FromDays(EnvironmentController.timeOfDay)).ToString("hhtt").TrimStart('0'); ;
         if(this.Text != text)
         {
             this.Text = text;
