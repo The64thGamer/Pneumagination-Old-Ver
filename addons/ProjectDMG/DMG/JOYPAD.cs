b036d8a10a437d736f7c5fcc04b74144fb2da782
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-04-05 03:06:16 -0500

    Added ProjectDMG plugin and converted it from C#Forms to Godot

---
+++ addons/ProjectDMG/DMG/JOYPAD.cs
@@ -0,0 +1,66 @@
+﻿using static ProjectDMG.Utils.BitOps;
+using Godot;
+
+namespace ProjectDMG {
+    public partial class JOYPAD : Node
+    {
+
+        private const int JOYPAD_INTERRUPT = 4;
+        private const byte PAD_MASK = 0x10;
+        private const byte BUTTON_MASK = 0x20;
+        private byte pad = 0xF;
+        private byte buttons = 0xF;
+
+
+        public void HandleInput()
+        {
+            if(Input.IsActionJustPressed("Arcade Up")){handleKeyDown(0x14);}
+            if(!Input.IsActionJustPressed("Arcade Up")){handleKeyUp(0x14);}
+            if(Input.IsActionJustPressed("Arcade Down")){handleKeyDown(0x18);}
+            if(!Input.IsActionJustPressed("Arcade Down")){handleKeyUp(0x18);}
+            if(Input.IsActionJustPressed("Arcade Left")){handleKeyDown(0x12);}
+            if(!Input.IsActionJustPressed("Arcade Left")){handleKeyUp(0x12);}
+            if(Input.IsActionJustPressed("Arcade Right")){handleKeyDown(0x11);}
+            if(!Input.IsActionJustPressed("Arcade Right")){handleKeyUp(0x11);}
+            if(Input.IsActionJustPressed("Arcade A")){handleKeyDown(0x21);}
+            if(!Input.IsActionJustPressed("Arcade A")){handleKeyUp(0x21);}
+            if(Input.IsActionJustPressed("Arcade B")){handleKeyDown(0x22);}
+            if(!Input.IsActionJustPressed("Arcade B")){handleKeyUp(0x22);}
+            if(Input.IsActionJustPressed("Arcade Start")){handleKeyDown(0x28);}
+            if(!Input.IsActionJustPressed("Arcade Start")){handleKeyUp(0x28);}
+            if(Input.IsActionJustPressed("Arcade Select")){handleKeyDown(0x24);}
+            if(!Input.IsActionJustPressed("Arcade Select")){handleKeyUp(0x24);}
+        }
+
+
+        internal void handleKeyDown(byte b) {
+            if ((b & PAD_MASK) == PAD_MASK) {
+                pad = (byte)(pad & ~(b & 0xF));
+            } else if((b & BUTTON_MASK) == BUTTON_MASK) {
+                buttons = (byte)(buttons & ~(b & 0xF));
+            }
+        }
+
+        internal void handleKeyUp(byte b) {
+            if ((b & PAD_MASK) == PAD_MASK) {
+                pad = (byte)(pad | (b & 0xF));
+            } else if ((b & BUTTON_MASK) == BUTTON_MASK) {
+                buttons = (byte)(buttons | (b & 0xF));
+            }
+        }
+
+        public void update(MMU mmu) {
+            byte JOYP = mmu.JOYP;
+            if(!isBit(4, JOYP)) {
+                mmu.JOYP = (byte)((JOYP & 0xF0) | pad);
+                if(pad != 0xF) mmu.requestInterrupt(JOYPAD_INTERRUPT);
+            }
+            if (!isBit(5, JOYP)) {
+                mmu.JOYP = (byte)((JOYP & 0xF0) | buttons);
+                if (buttons != 0xF) mmu.requestInterrupt(JOYPAD_INTERRUPT);
+            }
+            if ((JOYP & 0b00110000) == 0b00110000) mmu.JOYP = 0xFF;
+        }
+
+    }
+}
---
+++ addons/ProjectDMG/ProjectDMG.cs
@@ -0,0 +1,110 @@
+﻿using ProjectDMG.Utils;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Godot;
+
+namespace ProjectDMG {
+    public partial class ProjectDMG : TextureRect {
+
+
+        private CPU cpu;
+        private MMU mmu;
+        private PPU ppu;
+        private TIMER timer;
+        public JOYPAD joypad;
+
+        public bool power_switch;
+
+        long start = nanoTime();
+            long elapsed = 0;
+            int cpuCycles = 0;
+            int cyclesThisUpdate = 0;
+
+            Stopwatch timerCounter = new Stopwatch();
+
+        public override void _Process(double delta)
+        {
+            if (power_switch)
+            {
+                joypad.HandleInput();
+            }
+
+            EXECUTE();
+        }
+
+        public override void _Ready()
+        {            
+            POWER_ON("ROM.binary");
+            timerCounter.Start();
+        }
+
+        public void POWER_ON(string cartName) {
+            mmu = new MMU();
+            cpu = new CPU(mmu);
+            ppu = new PPU();
+            timer = new TIMER();
+            joypad = new JOYPAD();
+            Texture = ppu.finalScreen;
+            mmu.loadgameRom(cartName);
+
+            power_switch = true;
+        }
+
+        public void POWER_OFF() {
+            power_switch = false;
+        }
+
+        int fpsCounter;
+
+        public void EXECUTE() {
+            // Main Loop Work in progress
+
+            if (power_switch) {
+
+                if (timerCounter.ElapsedMilliseconds > 1000) {
+                    timerCounter.Restart();
+                    fpsCounter = 0;
+                }
+
+                if ((elapsed - start) >= 16740000) { //nanoseconds per frame
+                   start += 16740000;
+                    while (cyclesThisUpdate < Constants.CYCLES_PER_UPDATE) {
+                        cpuCycles = cpu.Exe();
+                        cyclesThisUpdate += cpuCycles;
+
+                        timer.update(cpuCycles, mmu);
+                        ppu.update(cpuCycles, mmu);
+                        joypad.update(mmu);
+                        handleInterrupts();
+                    }
+                    fpsCounter++;
+                    cyclesThisUpdate -= Constants.CYCLES_PER_UPDATE;
+                }
+
+                elapsed = nanoTime();
+            }
+            
+        }
+
+        private void handleInterrupts() {
+            byte IE = mmu.IE;
+            byte IF = mmu.IF;
+            for (int i = 0; i < 5; i++) {
+                if ((((IE & IF) >> i) & 0x1) == 1) {
+                    cpu.ExecuteInterrupt(i);
+                }
+            }
+
+            cpu.UpdateIME();
+        }
+
+        private static long nanoTime() {
+            long nano = 10000L * Stopwatch.GetTimestamp();
+            nano /= TimeSpan.TicksPerMillisecond;
+            nano *= 100L;
+            return nano;
+        }
+
+    }
+}
