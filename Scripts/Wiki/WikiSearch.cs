983fa872b9aca199e2777fb4721f8d188875cf9b
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-04-12 00:01:41 -0500

    Added searchable articles in the manual

---
+++ Scripts/Wiki/WikiSearch.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public partial class WikiSearch : LineEdit
+{
+	[Export] Label articleTitle;
+	[Export] RichTextLabel articleText;
+
+	public override void _Ready()
+	{
+		TextSubmitted += Search;
+		Search("Main Page");
+	}
+
+	void Search(String searchText)
+	{
+		XmlParser parser = new XmlParser();
+		parser.Open("res://Scripts/Wiki/WikiArticles.xml");
+		
+		string result = ParseXMLTitles(parser,searchText);
+		if(result != "")
+		{
+			articleTitle.Text = searchText;
+			articleText.Text = Tr(result);
+		}
+	}
+
+	string ParseXMLTitles(XmlParser parser, string searchText)
+	{
+		while (parser.Read() != Error.FileEof)
+		{
+			if (parser.GetNodeType() == XmlParser.NodeType.Element)
+			{
+				for (int idx = 0; idx < parser.GetAttributeCount(); idx++)
+				{
+					if(parser.GetAttributeName(idx) == "title" && parser.GetAttributeValue(idx) == searchText)
+					{
+						return "WIKI_ARTICLE_" + parser.GetAttributeValue(idx).ToUpper().Replace(' ','_');
+					}
+				}
+			}
+		}
+		return "";
+	}
+}
